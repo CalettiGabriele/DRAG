bedbdee4f643c9a92e4d48aebcb9d187c7b820a2
Gabriele <gabriele.caletti@outlook.it>
2024-03-12 22:18:26 +0100

    clear_llm_response; question_enhancer; pipe; flow

--- src/drag/groq/groq_chat.py
+++ src/drag/groq/groq_chat.py
@@ -13,9 +13,13 @@ class Groq_Chat(Drag):
         Drag.__init__(self, config=config)
 
         if "api_key" in config:
-            self.client = client = Groq(api_key=config["api_key"])
+            self.client = Groq(api_key=config["api_key"])
+        if "model" in config:
+            self.model = config["model"]
+        else:
+            self.model = "mixtral-8x7b-32768"
 
-    def submit_prompt(self, prompt, model="mixtral-8x7b-32768", temperature=0, **kwargs) -> str:
+    def submit_prompt(self, prompt, temperature=0, **kwargs) -> str:
         if prompt is None:
             raise Exception("Prompt is None")
         if len(prompt) == 0:
@@ -28,14 +32,30 @@ class Groq_Chat(Drag):
                 "content": prompt,
             }
         ],
-        model="mixtral-8x7b-32768",
+        model=self.model,
         temperature=0.,
         )
-
-        return chat_completion.choices[0].message.content
+        llm_response = self.clear_llm_response(llm_response=chat_completion.choices[0].message.content)
+        return llm_response
     
-    def clear_llm_response(self, prompt, **kwargs):
-        prompt = prompt.replace("```", "")
-        prompt = prompt.replace("python", "")
-        prompt = prompt.strip()
-        return prompt
+    def clear_llm_response(self, llm_response, **kwargs):
+        llm_response = self.extract_llm_response_between_triple_backticks(llm_response=llm_response)
+        llm_response = self.extract_characters_between_asterisks(llm_response=llm_response)
+        llm_response = llm_response.replace("```", "")
+        llm_response = llm_response.replace("python", "")
+        llm_response = llm_response.strip()
+        return llm_response
+    
+    def extract_llm_response_between_triple_backticks(self, llm_response):
+        start_index = llm_response.find('```')
+        end_index = llm_response.find('```', start_index + 3)
+        if (start_index != -1 or end_index != -1) and start_index != end_index:
+            return llm_response[start_index:end_index+3]
+        return llm_response
+        
+    def extract_characters_between_asterisks(self, llm_response):
+        start_index = llm_response.find("*")
+        end_index = llm_response.rfind("*")
+        if (start_index != -1 or end_index != -1) and start_index != end_index:
+            return llm_response[start_index+2:end_index-1]
+        return llm_response
